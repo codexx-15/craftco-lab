fd68590a626164ecd72412bdcdfd3b30cfb0acef
codexx-15 <lalgautam236@gmail.com>
2026-03-14 18:56:45 +0530

    Create App.tsx

---
+++ craftco-lab/src/app/App.tsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header"
+import HeroSection from "./components/HeroSection"
+import CategoryGrid from "./components/CategoryGrid"
+import FeaturedProducts from "./components/FeaturedProducts"
+import Footer from "./components/Footer"
+
+export default function App(){
+return(
+<div>
+
+<Header/>
+<HeroSection/>
+<CategoryGrid/>
+<FeaturedProducts/>
+<Footer/>
+
+</div>
+)
+}
