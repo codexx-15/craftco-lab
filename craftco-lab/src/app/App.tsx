e27e7acb530025a3a3194490563366b498838fa4
codexx-15 <lalgautam236@gmail.com>
2026-03-14 19:13:46 +0530

    Update App.tsx

--- craftco-lab/src/app/App.tsx
+++ craftco-lab/src/app/App.tsx
@@ -1,19 +1,34 @@
-import Header from "./components/Header"
-import HeroSection from "./components/HeroSection"
-import CategoryGrid from "./components/CategoryGrid"
-import FeaturedProducts from "./components/FeaturedProducts"
-import Footer from "./components/Footer"
+import { Header } from "./components/Header"
+import { AnnouncementBar } from "./components/AnnouncementBar"
+import { HeroSection } from "./components/HeroSection"
+import { CategoryGrid } from "./components/CategoryGrid"
+import { FeaturedProducts } from "./components/FeaturedProducts"
+import { ConnectSection } from "./components/ConnectSection"
+import { FAQSection } from "./components/FAQSection"
+import { Footer } from "./components/Footer"
 
-export default function App(){
-return(
-<div>
+export default function App() {
+return ( <div className="min-h-screen bg-[#fdfbfb]">
 
-<Header/>
-<HeroSection/>
-<CategoryGrid/>
-<FeaturedProducts/>
-<Footer/>
+```
+  <AnnouncementBar />
+
+  <Header />
+
+  <HeroSection />
+
+  <CategoryGrid />
+
+  <FeaturedProducts />
+
+  <ConnectSection />
+
+  <FAQSection />
+
+  <Footer />
 
 </div>
+```
+
 )
 }
